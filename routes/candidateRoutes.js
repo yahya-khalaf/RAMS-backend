f4f0509b972653f6f42cc502b52d4b055bcfa023
yahya khalaf <yahya.khalaf54@gmail.com>
2025-08-17 03:10:54 +0300

    ,

--- routes/candidateRoutes.js
+++ routes/candidateRoutes.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 const candidateController = require('../controllers/candidateController');
 const registerController = require('../controllers/registerController'); // NEW: Import new controller
+const authenticateToken = require('../authMiddleware');
 
 
 router.post('/update-state', candidateController.updateCandidateState);
 router.post('/', candidateController.createCandidate);
 router.get('/register/:token', registerController.getInstituteByToken);
-router.get('/', candidateController.getFilteredCandidates);
-router.delete('/:id', candidateController.deleteCandidate);
+router.get('/', authenticateToken, candidateController.getFilteredCandidates);
+router.delete('/:id', authenticateToken, candidateController.deleteCandidate);
 
 module.exports = router;
